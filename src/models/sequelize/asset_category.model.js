d08d64eefb7ece7df5ee89a9cf6607f3c0a955e8
Anahi Jacqueline Perez <anahiperezdsm@gmail.com>
2025-09-29 11:29:12 -0300

    realizo el modelo de tabla intermedia

--- src/models/sequelize/asset_category.model.js
+++ src/models/sequelize/asset_category.model.js
@@ -2,6 +2,24 @@ import { DataTypes } from "sequelize";
 
 export const AssetCategoryModel = sequelize.define("AssetCategory", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  asset_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'assets',
+      key: 'id'
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'categories',
+      key: 'id'
+    }
+  }
+}, {
+  timestamps: true,
 });
 
 // TODO: completar relaciones muchos a muchos entre Asset y Category mediante AssetCategory.
